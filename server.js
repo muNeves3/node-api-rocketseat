a1bdbe32a5df015bab9a248e365e57663abba779
muNeves3 <murilogrilosn@gmail.com>
2020-07-02 22:25:37 -0300

    product Schema and mongodbconnection created

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
+//inciando o app
 const app =express();
 
+//inciando o banco de dados
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi', 
+  { useNewUrlParser: true, useUnifiedTopology: true },
+);
+requireDir('./src/models');
+
+const Product = mongoose.model("Product");
+
+
+//primeira rota
 app.get("/", (req, res) => {
-  res.send("Hello, Rocketseat");
+  Product.create({title: 'React-Native', 
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native'
+  });
+  return res.send("Hello, Rocketseat");
 });
 
 app.listen(3001);
+
+//mongod to run mongoDB
